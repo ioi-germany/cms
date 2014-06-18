3c6d2df4811c3f00dd0c520d9298ab6c9133e595
Fabian Gundlach <320pointsguy@googlemail.com>
2014-06-18 21:55:08 +0200

    Set manager time limit to 15 in communication task type

--- cms/grading/tasktypes/Communication.py
+++ cms/grading/tasktypes/Communication.py
@@ -202,7 +202,7 @@ class Communication(TaskType):
         manager = evaluation_step_before_run(
             sandbox_mgr,
             manager_command,
-            job.time_limit,
+            15,
             0,
             allow_dirs=manager_allow_dirs,
             stdin_redirect="input.txt")
