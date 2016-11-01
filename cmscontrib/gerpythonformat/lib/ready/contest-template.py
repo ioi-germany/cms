eb10c43ef2a8ee8fe9a963c7ebb51787633d9ba6
Tobias Lenz <t_lenz94@web.de>
2016-11-01 18:00:05 +0100

    add file (template for cmsGerMakeTask) missing from last commit

---
+++ cmscontrib/gerpythonformat/lib/ready/contest-template.py
@@ -0,0 +1,9 @@
+description("~")
+timezone("Europe/Berlin")
+contest.lg = "Dummywettbewerb"
+
+from cmscontrib.gerpythonformat.templates.lg.LgTemplate import LgTemplate
+LgTemplate(contest)
+
+user_group("main", start=time("2000-01-01 00:00:00"), stop=time("2100-01-01 00:00:00"))
+test_user(user("testcontesttestuser", "secret", "Testcontest", "Testuser"))
