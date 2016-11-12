e951ae8d85b91d81dbe0dda666834eab557db8f0
Tobias Lenz <t_lenz94@web.de>
2016-11-12 19:27:03 +0100

    cmsTaskOverviewWebServer: trivial refactorings

--- cmstaskoverview/TaskOverviewWebServer.py
+++ cmstaskoverview/TaskOverviewWebServer.py
@@ -29,7 +29,7 @@ from tornado.ioloop import IOLoop
 from tornado.web import RequestHandler, Application
 
 from .Config import config
-from .Loader import TaskInfo
+from .TaskInfo import TaskInfo
 
 
 logger = logging.getLogger(__name__)
